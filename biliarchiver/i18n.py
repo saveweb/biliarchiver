d48f0de7aea9116bdf8c9306d0e4973ebee769cf
yzqzss <yzqzss@yandex.com>
2023-08-17 05:22:34 +0800

    fix: i18n: localedir

--- biliarchiver/i18n.py
+++ biliarchiver/i18n.py
@@ -1,15 +1,19 @@
 import gettext
 import locale
+from pathlib import Path
+import warnings
 
 default_lang, default_enc = locale.getdefaultlocale()
 default_lang = default_lang or "en"
-
 languages = ["en"] if not default_lang.lower().startswith("zh") else ["zh_CN"]
-
 appname = "biliarchiver"
 
+localedir = Path(__file__).parent / "locales"
+if not localedir.exists():
+    warnings.warn("Locales directory not found, i18n will not work.", RuntimeWarning)
+
 i18n = gettext.translation(
-    appname, localedir="biliarchiver/locales", fallback=True, languages=languages
+    appname, localedir=localedir, fallback=True, languages=languages
 )
 
 _ = i18n.gettext

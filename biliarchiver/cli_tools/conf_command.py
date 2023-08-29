086b1decb3587b31fd3bb32223145d611e1dc91d
Ovler <ovlertheyoung@gmail.com>
2023-08-29 14:11:05 +0800

    chore: Refactor import statements to avoid ci fail & update CI

--- biliarchiver/cli_tools/conf_command.py
+++ biliarchiver/cli_tools/conf_command.py
@@ -1,7 +1,6 @@
 import click
 from dataclasses import dataclass
 from biliarchiver.i18n import _
-from biliarchiver.config import _Config
 
 @click.command(help=click.style(_("将传入参数写入配置文件"), fg="cyan"))
 @click.option("--video_concurrency", "-v", type=click.INT, default=None, help=_("视频下载并发数"))
@@ -12,6 +11,7 @@ from biliarchiver.config import _Config
 @click.option("--cookies_file", "-c", type=click.STRING, default=None, help=_("cookies文件"))
 
 def config(**kwargs):
+    from biliarchiver.config import _Config
     config = _Config()
     for k, v in kwargs.items():
         if v is not None:
